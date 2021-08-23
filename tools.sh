417845bbde69657a855e3b3b09f10d6690403fc2
ictID13 <ictor13@gmail.com>
2021-08-23 13:28:54 +0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 ########################################
-
 K_GET_IP="--get_ip"
 K_CONNECT="--connect"
 K_DISCONNECT="--disconnect"
@@ -24,8 +22,9 @@ K_MONKEY="--monkey_test"
 K_CLEAR_DATA="--clear_data"
 K_RFAV="--readfromapkversion"
 K_HELP="--help"
+K_MATTER="--matter"
 
-ALL_KEYWORDS=( "${K_OPEN_FACT}" "${K_INSTALL}" "${K_RFAV}" "${K_GETPROP}" "${K_GET_IP}" "${K_MONKEY}" "${K_CONNECT}" "${K_DISCONNECT}" "${K_CONNECT2ALL_SEND_BROADCAST}" "${K_INSTALL_PAK}" "${K_LOGCAT}" "${K_UNINST}" "${K_UNINST_PAK}" "${K_UNINST_DIFF}" "${K_SEND}" "${K_STOP_APP}" "${K_GET_NAME}" "${K_GET_VERS}" "${K_DIFF_VERSION}" "${K_CLEAR_DATA}" "${K_FIND}" "${K_HELP}")
+ALL_KEYWORDS=("${K_MATTER}" "${K_OPEN_FACT}" "${K_INSTALL}" "${K_RFAV}" "${K_GETPROP}" "${K_GET_IP}" "${K_MONKEY}" "${K_CONNECT}" "${K_DISCONNECT}" "${K_CONNECT2ALL_SEND_BROADCAST}" "${K_INSTALL_PAK}" "${K_LOGCAT}" "${K_UNINST}" "${K_UNINST_PAK}" "${K_UNINST_DIFF}" "${K_SEND}" "${K_STOP_APP}" "${K_GET_NAME}" "${K_GET_VERS}" "${K_DIFF_VERSION}" "${K_CLEAR_DATA}" "${K_FIND}" "${K_HELP}")
 
 ###################Стартовые переменные#####################
 TOOL=~/.tool/
@@ -41,6 +40,7 @@ MODULES_UNINSTALL=(${TOOL}modules.uninstall)
 APKFILES=$(cat ${MODULES_INSTALL})
 TEMP_DIFF_NEW_OLD=(${TOOL}app_diff_new_old.txt)
 
+TEMP=(${TOOL}temp.txt)
 FIND=su
 PKG=store
 ACTIVITY=.ui.MainActivity
@@ -110,22 +110,15 @@ get_ip(){
 # nmap -p ${PORT:="5555"} -n ${RANGE:=192.168.0.100-254} --open |grep report |ssed 's/Nmap scan report for //' > ${WHITELIST}
 nmap -p ${PORT:="5555"} -n ${RANGE:=192.168.0.100-254} --open -oG - | awk '/Up$/{print $2}' > ${WHITELIST}
   echo "Список ip адресов с открытым портом ${PORT:="5555"}"
-cat ${WHITELIST}
+cat -n  ${WHITELIST}
 }
 
 #подключение adb connect
 connect(){
-#   echo "Try connect to $(head -n 1 ${WHITELIST}|tail -n 1)"
     echo -n "connect to ip?192.168.0.100    :"
     read -e IP
-#   timeout 1 adb connect $(head -n 1 ${WHITELIST}|tail -n 1):5555 2> /dev/null 1> /dev/null
-    timeout 1 adb connect ${IP:=192.168.0.100}:5555 #2> /dev/null 1> /dev/null
-    if [ $? -ne 0 ] ; then
-        echo "Failed to connect"
-        return 1
-    fi
-    echo "Success"
-    return 0
+    timeout 1 adb connect ${IP:=192.168.0.100}:5555 2> /dev/null 1> /dev/null
+    check
 }
 
 #подключение к ТВ по списку, отсылка broadcast, отключение
@@ -133,55 +126,68 @@ connect2all_send_broadcast(){
     for IPS in ${IP}; do
             disconnect
             tab
-            echo "Try connect to ${IPS}"
+            echo -n "Connecting to ${IPS} :"
             timeout 3 adb connect ${IPS}:5555  2> /dev/null 1> /dev/null
-            if [ $? -ne 0 ] ; then
-        echo "Failed to connect"
-    fi
-    echo "Success"
-#    return 0
-getprop
-sleep 1
-get_app_name_version  |grep abox.store.client
-#echo > ${MODULES_UNINSTALL}
-#get_app_name >> ${MODULES_UNINSTALL}
-#uninstall_pack_apk
-
+            check
+      send_broadcast_store_client
     done
   disconnect
     }
 
+matter(){
+cat /dev/null>${TEMP}
+      for IPS in ${IP}; do
+            disconnect
+            tab
+            echo -n "Connecting to ${IPS} :"
+            timeout 3 adb connect ${IPS}:5555  2> /dev/null 1> /dev/null
+            check
+            get_app_name_version |grep abox.store
+            sleep 1
+            echo -n "uninstall  :"
+            adb uninstall abox.store.client
+            sleep 1
+            echo "Done"
+            get_app_name_version |grep abox.store
+            sleep 1
+            echo -n "install  :"
+            install
+            echo "Done"
+            sleep 1
+            get_app_name_version |grep abox.store
+            sleep 1
+            echo -n "DO REBOOT"
+            adb shell reboot
+            echo "Done"
+    done
+disconnect
+}
 
 #Установка отдельных apk
 install(){
-  adb install -r /Users/Viktor/Documents/PROJECTS/STORE.CLIENT/APP/abox.store.client_20191226.apk
-sleep 1
-  disconnect
+  APP=/Users/Viktor/Downloads/abox.store.client.apk
+  adb install -r ${APP} 2>/dev/null 1>/dev/null
+  sleep 1
 }
 
 
 #Установка списка приложений
 install_pack_apk(){
-    cat /dev/null >|${MODULES_INSTALL}
-    ls -1 *.apk >> ${MODULES_INSTALL}
-
-# for INP in  $(ls -f *.tar.gz );do
+  cat /dev/null > ${MODULES_INSTALL}
+  ls -1 *.apk >> ${MODULES_INSTALL}
     for APK in $(cat ${MODULES_INSTALL}); do
             tab
-            echo "Installing  ${APK}"
-            adb install -r ${APK} # 2> /dev/null 1> /dev/null
-            if [ $? -ne 0 ] ; then
-        echo "Failed to install ${APK}"
-    fi
-
+            echo -n "Installing  ${APK}:"
+            adb install -r ${APK}  2> /dev/null 1> /dev/null
+            check
     done
-    }
+}
+
 
 #получение списка getprop
 getprop(){
-echo "Получение getprop:"
-adb shell getprop |grep -E "(sys.wildred.hw_id|sys.wildred.brand|ro.product.brand|ro.product.device|ro.product.model|sys.wildred.version|ro.build.version.min_supported_target_sdk|ro.build.version.sdk|ro.build.date.utc|ro.build.date|ro.sf.lcd_density|qemu.sf.lcd_density|vendor.display-size|smarttv.current.apk.activity|smarttv.current.apk.package|ro.main.version|ro.main.version.date)"
-# > ~/.tool/${IPS}.txt
+  echo  "Получение getprop:"
+  adb shell getprop |grep -E "(sys.wildred.hw_id|sys.wildred.brand|ro.product.brand|ro.product.device|ro.product.model|sys.wildred.version|ro.build.version.min_supported_target_sdk|ro.build.version.sdk|ro.build.date.utc|ro.build.date|ro.sf.lcd_density|qemu.sf.lcd_density|vendor.display-size|smarttv.current.apk.activity|smarttv.current.apk.package|ro.main.version|ro.main.version.date)"
 }
 
 
@@ -197,23 +203,27 @@ adb shell monkey -p ${PACKAGE:=com.family.atlas.launcher} --pct-touch 0 --pct-mo
         adb shell kill $(adb shell pidof com.android.commands.monkey)
 }
 
+
 #Остановка приложения abox.store
 stop_app(){
-    echo "Остановка abox.store.client"
-    adb shell am force-stop ${PKG:=abox.store.client}
-    }
+  echo  -n "Остановка abox.store.client :"
+  adb shell am force-stop ${PKG:=abox.store.client}
+  adb shell kill $(adb shell pidof abox.store.client)
+  echo "Done"
+}
+
 
 restart_app() {
-    stop_app
-    launch_activity
+  stop_app
+  launch_activity
 }
 
 #Установка prop
 setprop(){
-echo "Установка prop "
+  echo -n "Установка persist.sys.registered/persist.sys.licensed  :"
   adb shell setprop persist.sys.registered 1
   adb shell setprop persist.sys.licensed 1
-
+  echo "Done"
 }
 
 
@@ -235,52 +245,72 @@ uninstall(){
 
 # Удаление пакета diff новый/старый
 uninstall_diff_apk(){
-    echo "Удаление списка приложений  от обновленного к предыдущему состоянию:"
-      diff -E -b -B -w  ${APPCUR} ${APPDEF} | grep "<" | sed -e 's/^[ <]*//' > ${TEMP_DIFF_NEW_OLD}
-        for APK in $(cat ${TEMP_DIFF_NEW_OLD}); do
-          echo "Uninstalling apk ${APK}"
-            adb uninstall ${APK}
-        done
+  echo "Удаление списка приложений  от обновленного к предыдущему состоянию:"
+  diff -E -b -B -w  ${APPCUR} ${APPDEF} | grep "<" | sed -e 's/^[ <]*//' > ${TEMP_DIFF_NEW_OLD}
+    for APK in $(cat ${TEMP_DIFF_NEW_OLD}); do
+      echo "Uninstalling apk ${APK}"
+      adb uninstall ${APK}
+    done
 }
 
 
 #Удаление списка приложений
 uninstall_pack_apk(){
-    for APK in $(cat ${MODULES_UNINSTALL}); do
-            tab
-            echo "Uninstalling apk ${APK}"
-            adb shell pm uninstall -k ${APK} #2>/dev/null 1>/dev/null
-            if [ $? -ne 0 ] ; then
-        echo "Failed to uninstall ${APK}"
-    fi
+  for APK in $(cat ${MODULES_UNINSTALL}); do
+    tab
+    echo "Uninstalling apk ${APK} :"
+    adb shell pm uninstall -k ${APK}  2>/dev/null 1>/dev/null
+    check
+  done
+}
 
-    done
+#    echo "Получение списка имен пакетов"
+get_app_name() {
+  CMD='adb shell "pm list packages -f" | sed -e "s/==//"'
+  PACKAGES=$(eval ${CMD} | cut -f 2 -d "=")
+  #    echo  ${PACKAGES}
+  if [ ${#PACKAGES[@]} == 0 ]; then
+	  echo "No packages found"
+	  exit 0
+	fi
+  for PA in "${PACKAGES[@]}"; do
+    echo -e  "${PA}\n"
+  done
 }
 
 
-get_app_name() {
-#    echo "Получение списка имен пакетов"
-    CMD='adb shell "pm list packages -f" | sed -e "s/==//"'
-    PACKAGES=$(eval ${CMD} | cut -f 2 -d "=")
-#    echo  ${PACKAGES}
-    if [ ${#PACKAGES[@]} == 0 ]; then
-	    echo "No packages found"
-	    exit 0
-    fi
-    for PA in "${PACKAGES[@]}"; do
-	echo -e  "${PA}\n"
-
+#Получение установленных приложений и их версий с записью в файл
+get_app_name_version() {
+  echo "Получение списка пакетов и их версий"
+  CMD='adb shell "pm list packages -f" | ssed -e "s/==//"'
+  PACKAGES=($(eval ${CMD} | cut -f 2 -d "="))
+  if [ ${#PACKAGES[@]} == 0 ]; then
+	  echo "No packages found"
+#	   exit 0
+  fi
+  echo "Found packages: ${#PACKAGES[@]}"
+    for P in "${PACKAGES[@]}"; do
+	    NAME=$(echo "${P//[$'\t\r\n ']}")
+	    VERSIONS=($(adb shell dumpsys package $NAME | grep -i versionName | awk -F"=" '{print $2}'))
+		  echo "$NAME $VERSIONS"
     done
-    }
+}
 
 
 pull_file() {
-  echo "pull_apk"
-
+  PULL=$(/Users/Viktor/Documents/PROJECTS/ATLAS_launcher/\#atlas-02.04.2021/Atlas.Launcher-standard-launcher-release.apk)
+  echo -n "Pulling apk"
+  adb pull  ${PULL} 2>/dev/null 1>/dev/null
+  echo
+  pwd
 }
 
 push_file() {
-  echo "push_apk"
+ PUSH=$(/Users/Viktor/Documents/PROJECTS/ATLAS_launcher/\#atlas-02.04.2021/Atlas.Launcher-standard-launcher-release.apk)
+ EXDIR=$(/Users/Viktor/)
+  echo -n "Pushing  apk :"
+  adb push  ${PULL}  ${EXDIR} 2>/dev/null 1>/dev/null
+  echo "Done"
 }
 
 send_exec(){
@@ -288,10 +318,24 @@ send_exec(){
 }
 
 get_logcat(){
-    echo -n "Show logcat | grep by word?:"
-    read FD
-    adb shell logcat |  grep "${FD}"
-    }
+  cat /dev/null>${TEMP}
+    for IPS in ${IP}; do
+      disconnect
+      tab
+      echo -n "Connecting to ${IPS} :"
+      timeout 3 adb connect ${IPS}:5555  2> /dev/null 1> /dev/null
+      check
+      sleep 1
+      get_app_name_version |grep "abox.store.client"
+      echo -n "Receiving logcat: "
+      adb logcat |grep -E "(pushLOG|push|pushId = 243)" > ${TEMP} &
+      sleep 5
+      adb shell kill $(adb shell pidof logcat)
+      echo "Done"
+      cat ${TEMP}
+    done
+  disconnect
+  }
 
 launch_activity() {
     adb_all shell am start -n "$PKG/$ACTIVITY" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
@@ -303,30 +347,6 @@ tree() {
 prefs() {
     adb_all shell "cat /data/data/${PKG}/shared_prefs/*.xml"
 }
-
-
-
-#Получение установленных приложений и их версий с записью в файл
-get_app_name_version() {
-  echo "Получение списка пакетов и их версий"
-#    android_get_installed_packages.sh > ${APPDEF}
-#
-    CMD='adb shell "pm list packages -f" | ssed -e "s/==//"'
-    PACKAGES=($(eval ${CMD} | cut -f 2 -d "="))
-
-    if [ ${#PACKAGES[@]} == 0 ]; then
-	    echo "No packages found"
-#	    exit 0
-    fi
-
-    echo "Found packages: ${#PACKAGES[@]}"
-
-    for P in "${PACKAGES[@]}"; do
-	    NAME=$(echo "${P//[$'\t\r\n ']}")
-	    VERSIONS=($(adb shell dumpsys package $NAME | grep -i versionName | awk -F"=" '{print $2}'))
-		echo "$NAME $VERSIONS"
-    done
-    }
 
 
 launch_activity(){
@@ -346,23 +366,35 @@ check_apps_version_vs(){
 
 #Отправка broadcast'а для того что бы дернуть сервис  abox.store.client
 send_broadcast_store_client(){
-      echo "Broadcast for abox.store.client already send"
-      adb shell am broadcast -a abox.store.client.ACTION_START -n abox.store.client/.receiver.StoreBroadcastReceiver
+      echo -n "Broadcast for abox.store.client : "
+      adb shell am broadcast -a abox.store.client.ACTION_START -n abox.store.client/.receiver.StoreBroadcastReceiver  2> /dev/null 1> /dev/null
+      if [ $? -ne 0 ] ; then
+        echo "Failed "
+        else
+        echo "Sended"
+    fi
       }
+
+check(){
+if [ $? -ne 0 ] ; then
+        echo "Failed"
+        else
+        echo "Success"
+    fi
+}
+#Последовательный перебор всех  вариантов вызова factory_menu
 open_factory(){
-tab
-echo "Последовательный перебор всех  вариантов вызова factory_menu "
-tab
-echo "send HiKeen(RTK2851/RTK2842) SOURCE>2580"
+echo  -n "send HiKeen(RTK2851/RTK2842) SOURCE>2580 : "
   adb shell am start -n 'com.hikeen.factorymenu/com.hikeen.factorymenu.FactoryMenuActivity'  2> /dev/null 1> /dev/null
+  check
   sleep 1
-echo ""
-echo "send CVTE(MTK55xx/SK506/SK706) HOME.SOURCE>ATV>MENU>1147"
+echo -n "send CVTE(MTK55xx/SK506/SK706) HOME.SOURCE>ATV>MENU>1147 : "
   adb shell am startservice -n com.cvte.fac.menu/com.cvte.fac.menu.app.TvMenuWindowManagerService --es com.cvte.fac.menu.commmand com.cvte.fac.menu.commmand.factory_menu  2> /dev/null 1> /dev/null
+  check
   sleep 1
-echo ""
-echo "send KTC(6681) SOURCE>ATV>MENU>8202"
-  adb shell am start -n kgzn.factorymenu.ui/mediatek.tvsetting.factory.ui.kgznfactorymenu.FactoryMenuActivity  2> /dev/null 1> /dev/null
+echo -n "send KTC(6681) SOURCE>ATV>MENU>8202 :"
+  adb shell am start -n kgzn.factorymenu.ui/mediatek.tvsetting.factory.ui.kgznfactorymenu.FactoryMenuActivity  2> /dev/null  1> /dev/null
+  check
   sleep 1
 tab
 }
@@ -477,6 +509,10 @@ for arg in "$@"; do
             ;;
       ${K_RFAV})
             readfromapkversion
+            shift 1
+            ;;
+      ${K_MATTER})
+            matter
             shift 1
             ;;
       ${K_OPEN_FACT})
